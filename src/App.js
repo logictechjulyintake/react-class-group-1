0014e7442437272e293c11944e5736cb39200323
logictechjulyintake <julyintakelogictech@gmail.com>
2023-08-29 08:23:37 +0545

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
-import AllEg from './components/AllEg';
+import FetchAPIEg from './components/FetchAPIEg';
+import MyForm from './components/MyForm';
+import Banner from './components/Banner';
+import Home from './components/Home';
+import APIEgTwo from './components/APIEgTwo';
+import Input from './components/Input';
+import MapOrLoop from './components/MapOrLoop';
+import UseRefEg from './components/UseRefEg';
+import UseEffecEg from './components/UseEffecEg';
 
 
 function App() {
   return <>
-    <AllEg />
+    {/* <AllEg /> */}
+
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/apieg" element={<FetchAPIEg />} />
+        <Route path="/apiegtwo" element={<APIEgTwo />} />
+        <Route path="/banner" element={<Banner />} />
+        <Route path="/form" element={<MyForm />} />
+        <Route path="/banner" element={<Banner />} />
+        <Route path="/input" element={<Input />} />
+        <Route path="/map" element={<MapOrLoop />} />
+        <Route path="/useref" element={<UseRefEg />} />
+        <Route path="/useeffecteg" element={<UseEffecEg />} />
+      </Routes>
+    </BrowserRouter>
+
   </>
 }
 
